d0aaeb9c4206e566f5a0086940c5198f387f28b7
erdem şen <erde_msn@windowslive.com>
2018-11-28 17:13:22 +0300

    Event handlers

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
-const path=require('path');
+const path = require('path');
+const http = require('http');
 const express = require('express');
+var favicon = require('serve-favicon');
+const socketIO = require('socket.io');
 
-const publicPath=path.join(__dirname,'../public');
-
-var app=express();
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io= socketIO(server);
 
 app.use(express.static(publicPath));
+app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
 
-const port = process.env.PORT || 3000;
+io.on('connection',(socket)=>{
+  console.log('New user connected');
+});
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
