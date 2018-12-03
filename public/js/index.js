9a596c8afc07d320da5c55e9d3c96834f5f6b208
erdem şen <erde_msn@windowslive.com>
2018-12-03 21:31:27 +0300

    add css for chat app

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,14 @@
 var socket = io();
 
-socket.on('connect', function () {
+socket.on('connect', function() {
   console.log('Connected to server');
 });
 
-socket.on('disconnect', function () {
+socket.on('disconnect', function() {
   console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function (message) {
+socket.on('newMessage', function(message) {
   console.log('newMessage', message);
   var li = jQuery('<li></li>');
   li.text(`${message.from}: ${message.text}`);
@@ -16,32 +16,42 @@ socket.on('newMessage', function (message) {
   jQuery('#messages').append(li);
 });
 
-jQuery('#message-form').on('submit', function (e) {
-  e.preventDefault();
+socket.on('newLocationMessage',function(message){
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>');
 
+  li.text(`${message.from}: `);
+  a.attr('href',message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+  var messageTextBox=jQuery('[name=message]');
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
-  }, function () {
-    console.log('form a girdi');
+    text: messageTextBox.val()
+  }, function() {
+    messageTextBox.val('')
   });
 });
 
-// $('#message-form').submit(function(event){
-//   event.preventDefault();
-//   socket.emit('createMessage', {
-//     from: 'User',
-//     text: jQuery('[name=message]').val()
-//   }, function () {
-//
-//   });
-// });
-//
-// function submitFrom(){
-//   socket.emit('createMessage', {
-//     from: 'User',
-//     text: jQuery('[name=message]').val()
-//   }, function () {
-//
-//   });
-// }
+var locationButton = jQuery('#send-location');
+
+locationButton.on('click',function(){
+  if(!navigator.geolocation){
+    return alert('Geolocation not supported by your browser.');
+  }
+  locationButton.attr('disabled','disabled').text('Sending Location...');;
+  navigator.geolocation.getCurrentPosition(function(position){
+    locationButton.removeAttr('disabled').text('Send Location');
+    socket.emit('createLocationMessage',{
+      latitude:position.coords.latitude,
+      longitude:position.coords.longitude
+    });
+  },function(){
+    locationButton.removeAttr('disabled').text('Send Location');
+    alert('Unable to fetch location');
+  });
+});
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage,generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -10,5 +10,18 @@ describe('generateMessage', () => {
 
     expect(message.createdAt).toBeA('number');
     expect(message).toInclude({from, text});
+  });
+});
+
+describe('generateLocationMessage',()=>{
+  it('should generate correct location object',()=>{
+    var from ='Deb';
+    var lat=12;
+    var long=23;
+    var url='https://www.google.com/maps?q=12,23';
+    message=generateLocationMessage(from,lat,long);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from,url});
   });
 });
